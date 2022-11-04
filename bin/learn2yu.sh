cd49ee1201b446efa6d0c23d8ab84cdd80935f08
jnishii <nishii@yamaguchi-u.ac.jp>
2022-11-04 19:34:02 +0900

    update baseurl

--- bin/learn2yu.sh
+++ bin/learn2yu.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-hugo -s ${HOME}/Sites/hugo-learn/ \
-	-b http://ds0n.cc.yamaguchi-u.ac.jp/~nishii/notebook \
-	-d /Volumes/web/nishii@ds0n/public_html/notebook
 
+SOURCE=${HOME}/Sites/hugo-learn/
+DEST=${HOME}/mnt/web/nishii@ds0n/public_html/notebook
+
+hugo -s ${HOME}/Sites/hugo-learn/ \
+        -b /~jun/notebook \
+        -d ${DEST} 
+#        --cleanDestinationDir
+
+#                       -b http://bcl.sci.yamaguchi-u.ac.jp/~jun/ \
+#               -b http://ds0n.cc.yamaguchi-u.ac.jp/~nishii/ \
